e2bbec889c28166cdd4a40ac30de1c4ef0670d33
jrajav <jrajav@gmail.com>
2014-06-02 22:09:40 -0500

    Init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ssocore-consumer-example
 ========================
+
+Requires node and npm, and [ssocore](https://github.com/jrajav/ssocore) running.
+
+Run `npm install` to install or update dependencies, then `node app.js` to run the server. Visit the server's root in a browser to see ssocore authentication in action.
---
+++ app.js
@@ -0,0 +1,40 @@
+var _ = require('lodash')
+var request = require('request')
+var consolidate = require('consolidate')
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var express = require('express')
+var app = express()
+var util = require('util')
+
+
+var PORT = 8050;
+var THIS = 'http://localhost:' + PORT + '/'
+
+var AUTH_URL = 'http://localhost:8000/auth'
+var LOGIN_URL = 'http://localhost:8000/login'
+
+
+app.engine('html', consolidate.lodash)
+app.set('view engine', 'html')
+app.set('views', __dirname)
+
+
+app.get('/', function (req, res) {
+	req.pipe(
+		request(
+			{ uri: AUTH_URL, json: true},
+			function (error, response, user) {
+				if (!error && response.statusCode == 200) {
+					res.render('index', {user: user})
+				} else {
+					res.redirect(LOGIN_URL + '?returnTo=' + encodeURIComponent(THIS))
+				}
+			}
+		)
+	)
+})
+
+var server = app.listen(PORT, function () {
+	console.log('Listening on port %d', server.address().port)
+})
